8fe41b5263d978446695088a44248acd9c415d11
Manuel Tiago Pereira <manuel@mtpereira.com>
2024-02-16 17:58:32 +0000

    Improve twoSum execution time

--- twoSum/main.go
+++ twoSum/main.go
@@ -1,19 +1,17 @@
 package main
 
-import (
-	"slices"
-)
-
 // twoSum implements a solution for
 // https://leetcode.com/problems/two-sum/
 // For each number, calculate the difference between it and the target,
 // then look for the difference in nums, and return index of both.
 func twoSum(nums []int, target int) []int {
+	seen := make(map[int]int, len(nums))
 	for i, n := range nums {
 		d := target - n
-		if j := slices.Index[[]int, int](nums, d); j != -1 && j != i {
-			return []int{i, j}
+		if j, ok := seen[d]; ok && j != i {
+			return []int{j, i}
 		}
+		seen[n] = i
 	}
 	return nil
 }
